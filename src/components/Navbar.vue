19fb3b5a0746e0532b5fcba0c4c71f20ae6b520a
hugo <hugo.lavergne@edu.devinci.fr>
2021-01-16 01:04:15 +0100

    init boilerplate

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <router-link :to="{name: 'home'}" class="navbar-brand" href="#">Vue-js Boilerplate</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+        <router-link :to="{name: 'home'}"  class="nav-item nav-link" href="#">Home</router-link>
+        <router-link :to="{name: 'about'}"  class="nav-item nav-link" href="#">About</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
